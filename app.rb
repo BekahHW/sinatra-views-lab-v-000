f3ff78c0fe8ac35d61fbd7ee992a344523b5fca8
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-10-03 09:59:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,15 +5,15 @@ class App < Sinatra::Base
 	end
 
 	get '/hello' do
-		erb :index
+		erb :hello
 	end
 	
-		get '/goodbye' do
-		erb :index
+	get '/goodbye' do
+		erb :goodbye
 	end
 	
-		get '/date' do
-		erb :index
+	get '/date' do
+		erb :date
 	end
 
 end
